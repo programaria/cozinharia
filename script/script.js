3bc97012237b5d837cfcee62946430586b0414ff
JulianaAmoasei <late.parrot@gmail.com>
2018-10-06 18:19:18 -0300

    scroll do cardapio parcialmente ok, porem ainda com bugs

--- script/script.js
+++ script/script.js
@@ -2,6 +2,34 @@
 var menuItem = document.getElementsByClassName("has-submenu")[0];
 var subMenu = menuItem.getElementsByClassName("submenu")[0];
 
+// submenu do header usando jquery
+$('.has-submenu').click(function (event) {
+  $(this).toggleClass('active');
+});
+
+// hover no cardapio gallery da home
+$('.cardapio-gallery figure').mouseover(function (event) {
+  $(this).addClass('active');
+});
+
+$('.cardapio-gallery figure').mouseleave(function (event) {
+  $(this).removeClass('active');
+});
+
+// navegação entre os cardapios almoço, jantar, bebidas
+$("#cardapio a").on("click", function() {
+  var $place = $(this).attr("href");
+  $("#cardapio a").removeClass('active');
+  $(this).addClass('active');
+
+  var top = ($place).offsetTop;
+  window.scrollTo(0, top);  
+
+
+
+});
+
+
 // // definindo os estilos pelo JS
 // menuItem.onclick = function() {
 //   subMenu.style.display = "block";
@@ -22,39 +50,3 @@ var subMenu = menuItem.getElementsByClassName("submenu")[0];
 // // menuItem.addEventListener("click", function() {
 //   menuItem.className += " active";
 // });
-
-// submenu do header usando jquery
-$('.has-submenu').click(function (event) {
-  $(this).toggleClass('active');
-});
-
-// hover no cardapio gallery da home
-$('.cardapio-gallery figure').mouseover(function (event) {
-  $(this).addClass('active');
-});
-
-$('.cardapio-gallery figure').mouseleave(function (event) {
-  $(this).removeClass('active');
-});
-
-// navegação entre os cardapios almoço, jantar, bebidas
-$("#cardapio a").on("click", function() {
-  var $place = $(this).attr("href");
-  $("#cardapio a").removeClass('active');
-  $(this).addClass('active');
-  $("body").animate({
-    scrollTop: $($place).offset().top - 346}, 600);
-    return false;
-});
-
-
-// criando um objeto personalizado
-// var cachorro = new Object();
-//
-// cachorro.pelo = "macio";
-// cachorro.idade = 2;
-// cachorro.tamanho = "médio";
-// cachorro.raca = "vira-lata";
-// cachorro.cor = "marrom manchado";
-// cachorro.nome = "totó";
-// cachorro.late = function () {alert("auau!")};
